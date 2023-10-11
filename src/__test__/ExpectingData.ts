4b5c1196c9cd0459fa7d676b98d8ab95b0ba1b2f
ares30841167 <areschentw@outlook.com>
2023-10-11 12:56:29 +0800

    test: add SortSystem test cases

---
+++ src/__test__/ExpectingData.ts
@@ -0,0 +1,87 @@
+import { BookInfo } from "@externals/simple-db";
+
+export const AscendingBookInfo: BookInfo[] = [
+    {
+        "ISBN": "680-71-48243-17-0",
+        "author": "Stephenie Meyer",
+        "title": "Alice Adventures in Wonderland",
+    },
+    {
+        "ISBN": "712-03-87188-05-4",
+        "author": "Willain Bradbury",
+        "title": "Bone of fire",
+    },
+    {
+        "ISBN": "572-70-62221-82-2",
+        "author": "Henry James",
+        "title": "Emma Story",
+    },
+    {
+        "ISBN": "255-03-71788-05-4",
+        "author": "Ray Bradbury",
+        "title": "Game of Thrones I",
+    },
+    {
+        "ISBN": "148-71-77362-42-3",
+        "author": "J. R. R. Tolkien",
+        "title": "Game of Thrones II",
+    },
+    {
+        "ISBN": "746-25-05830-50-7",
+        "author": "Ernest Hemingway",
+        "title": "One Thousand and One Nights",
+    },
+    {
+        "ISBN": "776-33-13328-46-3",
+        "author": "Doris Lessing",
+        "title": "The Lord of The Rings",
+    },
+    {
+        "ISBN": "774-13-13326-60-1",
+        "author": "Danielle Steel",
+        "title": "To Kill a Mockingbird",
+    },
+]
+
+export const DescendingBookInfo: BookInfo[] = [
+    {
+        "ISBN": "774-13-13326-60-1",
+        "author": "Danielle Steel",
+        "title": "To Kill a Mockingbird",
+    },
+    {
+        "ISBN": "776-33-13328-46-3",
+        "author": "Doris Lessing",
+        "title": "The Lord of The Rings",
+    },
+    {
+        "ISBN": "746-25-05830-50-7",
+        "author": "Ernest Hemingway",
+        "title": "One Thousand and One Nights",
+    },
+    {
+        "ISBN": "148-71-77362-42-3",
+        "author": "J. R. R. Tolkien",
+        "title": "Game of Thrones II",
+    },
+    {
+        "ISBN": "255-03-71788-05-4",
+        "author": "Ray Bradbury",
+        "title": "Game of Thrones I",
+    },
+    {
+        "ISBN": "572-70-62221-82-2",
+        "author": "Henry James",
+        "title": "Emma Story",
+    },
+    {
+        "ISBN": "712-03-87188-05-4",
+        "author": "Willain Bradbury",
+        "title": "Bone of fire",
+    },
+    {
+        "ISBN": "680-71-48243-17-0",
+        "author": "Stephenie Meyer",
+        "title": "Alice Adventures in Wonderland",
+    },
+]
